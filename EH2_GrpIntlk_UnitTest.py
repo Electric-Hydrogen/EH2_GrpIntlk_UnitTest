358a8f65af8d6a33612fef254442ee8325e0faf6
Erik Johansson <ejohansson@eh2.com>
2024-10-24 11:58:15 -0700

    Updated to loop through all child interlocks in same test

--- EH2_GrpIntlk_UnitTest.py
+++ EH2_GrpIntlk_UnitTest.py
@@ -10,7 +10,7 @@ UDT_Type = 'P_Intlk_UDT'
 UDT_Name = 'TestGrp_IntlkArr'
 Test_PLC_Name = 'EH2_GrpIntlk'
 P_Intlk_Name = 'Test_Intlk'
-WAIT_DELAY = 0.5
+WAIT_DELAY = 0.05
 
 plc = LogixDriver(commpath, init_tags=True,init_program_tags=True)
 
@@ -37,13 +37,14 @@ def write_and_check_tag(plc,tag,value):
     if write_status[3] == None: # we have a good write
         '''
         while True:
+            time.sleep(WAIT_DELAY)
             read_status = plc.read(tag)
 
             # check data matches
             if read_status[1] == write_status[1]:
-                return True
-        '''
+        '''        
         return True
+        
     else:
         return False
 
@@ -203,11 +204,15 @@ class TestAOI(unittest.TestCase):
 
         connect_to_plc()
 
+        write_and_check_tag(plc,'Test_Bypass',0)
+
     @classmethod
     def tearDownclass(cls):
 
         write_and_check_tag(plc,'Test_Start',0)
         write_and_check_tag(plc,'StepWord',1)
+
+        write_and_check_tag(plc,'Test_Bypass',1)
 
         wait_for_ready()
 
@@ -242,42 +247,107 @@ class TestAOI(unittest.TestCase):
     #@unittest.skipUnless(plc.connected, "PLC not connected")
     def test_1_check_plc_file_loaded(self):
         self.assertEqual(plc.get_plc_name(),Test_PLC_Name,'Check correct file is loaded on bench PLC')
-
+    
     def test_2_check_inputs_non_bypassable_non_latch(self):
 
         for i in range(16):
+            groupnum = i+1
 
-            intlk_mask = 2**i
-            #print(intlk_mask)
+            print('Checking group ' + str(groupnum))
 
-            # write to first interlock in child interlock group
-            intlkgrp_tagwrite = UDT_Name + '[' + str(i) + '].Inp_Intlk'
-            write_and_check_tag(plc,intlkgrp_tagwrite,0b0000_0000_0000_0001)
+            for j in range(15):
+                intlk_mask = 2**i
 
-            # write to interlock in P_Intlk
-            if i < 10:
-                intlk_tagwrite = P_Intlk_Name + '.Inp_Intlk0' + str(i)
-            else:
-                intlk_tagwrite = P_Intlk_Name + '.Inp_Intlk' + str(i)
-            write_and_check_tag(plc,intlk_tagwrite,1)
+                child_trip = 2**j
 
-            #start the test
-            run_test()
+                print('Checking Input ' + str(j))
 
-            # tests for group interlock AOI
-            self.assertFalse(read_tag(plc,AOI_Name + '_SnapShot.Sts_IntlkOK'),'Bypassable Interlocks OK')
-            self.assertFalse(read_tag(plc,AOI_Name + '_SnapShot.Sts_NBIntlkOK'),'Non Bypassable Interlocks OK')
-            self.assertEqual(read_tag(plc,AOI_Name + '_SnapShot.Sts_Intlk'),intlk_mask,'Interlock Status')
-            self.assertEqual(read_tag(plc,AOI_Name + '_SnapShot.Sts_FirstOut'),intlk_mask,'First Out Status')
-            self.assertEqual(read_tag(plc,AOI_Name + '_SnapShot.Val_FirstOutTxt'),str(1+16*i),'First Out Text Status')
+                # write to first interlock in child interlock group
+                intlkgrp_tagwrite = UDT_Name + '[' + str(i) + '].Inp_Intlk'
+                write_and_check_tag(plc,intlkgrp_tagwrite,child_trip)
 
-            # tests to compare group interlock with P_Intlk
-            self.assertEqual(read_tag(plc,AOI_Name + '_SnapShot.Sts_Intlk'),read_tag(plc,P_Intlk_Name + '_SnapShot.Sts_Intlk'),'Interlock Status Matches for Group ' + str(i+1))
-            self.assertEqual(read_tag(plc,AOI_Name + '_SnapShot.Sts_IntlkOK'),read_tag(plc,P_Intlk_Name + '_SnapShot.Sts_IntlkOK'),'Interlock OK Status Matches for Group ' + str(i+1))
-            self.assertEqual(read_tag(plc,AOI_Name + '_SnapShot.Sts_NBIntlkOK'),read_tag(plc,P_Intlk_Name + '_SnapShot.Sts_NBIntlkOK'),'Non Bypassable Interlock Status Matches for Group ' + str(i+1))
+                # write to interlock in P_Intlk
+                if i < 10:
+                    intlk_tagwrite = P_Intlk_Name + '.Inp_Intlk0' + str(i)
+                else:
+                    intlk_tagwrite = P_Intlk_Name + '.Inp_Intlk' + str(i)
+                write_and_check_tag(plc,intlk_tagwrite,1)
 
-            reset_test()
+                #start the test
+                run_test()
 
+                # tests for group interlock AOI
+                self.assertFalse(read_tag(plc,AOI_Name + '_SnapShot.Sts_IntlkOK'),'Bypassable Interlocks OK')
+                self.assertFalse(read_tag(plc,AOI_Name + '_SnapShot.Sts_NBIntlkOK'),'Non Bypassable Interlocks OK')
+                self.assertEqual(bin(abs(int(read_tag(plc,AOI_Name + '_SnapShot.Sts_Intlk')))),bin(int(intlk_mask)),'Interlock Status')
+                self.assertEqual(bin(abs(int(read_tag(plc,AOI_Name + '_SnapShot.Sts_FirstOut')))),bin(int(intlk_mask)),'First Out Status')
+                self.assertEqual(read_tag(plc,AOI_Name + '_SnapShot.Val_FirstOutTxt'),str((j+1)+16*i),'First Out Text Status')
+
+                # tests to compare group interlock with P_Intlk
+                self.assertEqual(read_tag(plc,AOI_Name + '_SnapShot.Sts_Intlk'),read_tag(plc,P_Intlk_Name + '_SnapShot.Sts_Intlk'),'Interlock Status Matches for Group ' + str(groupnum))
+                self.assertEqual(read_tag(plc,AOI_Name + '_SnapShot.Sts_IntlkOK'),read_tag(plc,P_Intlk_Name + '_SnapShot.Sts_IntlkOK'),'Interlock OK Status Matches for Group ' + str(groupnum))
+                self.assertEqual(read_tag(plc,AOI_Name + '_SnapShot.Sts_NBIntlkOK'),read_tag(plc,P_Intlk_Name + '_SnapShot.Sts_NBIntlkOK'),'Non Bypassable Interlock Status Matches for Group ' + str(groupnum))
+
+                reset_test()
+    '''
+    def test_1_check_inputs_bypassable_non_latch(self):
+
+        for i in range(16):
+            groupnum = i+1
+
+            print('Checking group ' + str(groupnum))
+
+            for j in range(15):
+                # EH2_GrpInltk Setup
+
+                intlk_mask = 2**i
+                child_mask = 2**j
+
+                # set bypasses
+                write_and_check_tag(plc,AOI_Name + '.Inp_BypActive',1)
+                write_and_check_tag(plc,AOI_Name + '.Cfg_Bypassable',intlk_mask)
+                write_and_check_tag(plc,AOI_Name + '.MSet_Bypass',intlk_mask)
+
+                print('Checking Input ' + str(j))
+
+
+                tagwrite_child_base = UDT_Name + '[' + str(i) + ']'
+
+                # write to first interlock in child interlock group
+                write_and_check_tag(plc,tagwrite_child_base + '.Inp_Intlk',child_mask)
+
+                # P_Intlk Setup
+
+                # set bypasses
+                write_and_check_tag(plc,P_Intlk_Name + '.Inp_BypActive',1)
+                write_and_check_tag(plc,P_Intlk_Name + '.Cfg_Bypassable',intlk_mask)
+                write_and_check_tag(plc,P_Intlk_Name + '.MSet_Bypass',intlk_mask)
+
+                # write to interlock in P_Intlk
+                if i < 10:
+                    intlk_tagwrite = P_Intlk_Name + '.Inp_Intlk0' + str(i)
+                else:
+                    intlk_tagwrite = P_Intlk_Name + '.Inp_Intlk' + str(i)
+                write_and_check_tag(plc,intlk_tagwrite,1)
+
+                #start the test
+                run_test()
+
+                # tests for group interlock AOI
+                self.assertTrue(read_tag(plc,AOI_Name + '_SnapShot.Sts_BypActive'),'Bypass Enabled')
+                self.assertFalse(read_tag(plc,AOI_Name + '_SnapShot.Sts_IntlkOK'),'Bypassable Interlocks OK')
+                self.assertTrue(read_tag(plc,AOI_Name + '_SnapShot.Sts_NBIntlkOK'),'Non Bypassable Interlocks OK')
+                self.assertEqual(bin(abs(int(read_tag(plc,AOI_Name + '_SnapShot.Sts_Intlk')))),bin(int(intlk_mask)),'Interlock Status')
+                self.assertEqual(bin(abs(int(read_tag(plc,AOI_Name + '_SnapShot.Sts_FirstOut')))),bin(int(intlk_mask)),'First Out Status')
+                #self.assertEqual(read_tag(plc,AOI_Name + '_SnapShot.Val_FirstOutTxt'),str((j+1)+16*i),'First Out Text Status')
+
+                # tests to compare group interlock with P_Intlk
+                self.assertEqual(read_tag(plc,AOI_Name + '_SnapShot.Sts_Intlk'),read_tag(plc,P_Intlk_Name + '_SnapShot.Sts_Intlk'),'Interlock Status Matches for Group ' + str(groupnum))
+                self.assertEqual(read_tag(plc,AOI_Name + '_SnapShot.Sts_IntlkOK'),read_tag(plc,P_Intlk_Name + '_SnapShot.Sts_IntlkOK'),'Interlock OK Status Matches for Group ' + str(groupnum))
+                self.assertEqual(read_tag(plc,AOI_Name + '_SnapShot.Sts_NBIntlkOK'),read_tag(plc,P_Intlk_Name + '_SnapShot.Sts_NBIntlkOK'),'Non Bypassable Interlock Status Matches for Group ' + str(groupnum))
+
+                reset_test()
+    '''
 
 
 if __name__ == '__main__':
